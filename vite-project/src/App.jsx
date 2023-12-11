ccae35bb697dce672ab73d6b6d1e33f4476197cc
Ivan Gonzalez Vargas <ronin7_@hotmail.com>
2023-12-10 18:47:33 -0600

    add_05:sparkles:

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css"
 
+
 // COMPONENTS
 import Header from "./components/Header/Header";
 import UserCard from "./components/UserCard/UserCard";
--- vite-project/src/components/ItemListContainer/ItemListContainer.jsx
+++ vite-project/src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ItemListContainer = () => {
     return (
-        <div style={{ backgroundColor: "darkorange", padding: "5px", marginTop: "7px" }}>
+        <div style={{ backgroundColor: "darkorange", padding: "5px", marginTop: "380px", position: "fixed", zIndex: "1" }}>
             <h2>U Need IT, We Got IT!!</h2>
         </div>
     )
